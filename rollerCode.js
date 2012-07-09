39d40720dfe3214e8dc6aa3ed0bdba08b25795b1
David Moran <fordmadoxfraud@gmail.com>
2012-07-09 00:51:38 -0400

    added a more complex roller

--- rollerCode.js
+++ rollerCode.js
@@ -1,5 +1,20 @@
-var rollDice = function(d) {
+//basic roll of a single die
+var rollDie = function(d) {
 	return Math.floor(Math.random()*d)+1;
 }
 
-alert(rollDice(6));
+//roll of a number of dice in the format 3d6+2, e.g.
+var complexRoll = function(numDice,d,mod) {
+	var total = 0;
+	var numDice = numDice;
+	for (var i = numDice; i > 0; i--) {
+		var roll = rollDie(d);
+		if (roll === d) {
+			numDice++;
+		}
+		total += roll;
+	}
+	return total+mod;
+}
+
+alert(complexRoll(3,6,2));
