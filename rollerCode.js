dddc1f45528636ee9d70f9145048ab7c25f268f1
David Moran <fordmadoxfraud@gmail.com>
2012-07-09 01:58:06 -0400

    changed functions to object methods

--- rollerCode.js
+++ rollerCode.js
@@ -1,29 +1,39 @@
-//basic roll of a single die
-var rollDie = function(d) {
-	return Math.floor(Math.random()*d)+1;
-}
+function MyRoll() {
+	this.highRoll = 0;
 
-//roll of a number of dice in the format 3d6+2, e.g.
-var complexRoll = function(numDice,d,mod) {
-	var total = 0;
-	var numDice = numDice;
-	for (var i = numDice; i > 0; i--) {
-		var roll = rollDie(d);
-		if (roll === d) {
-			i++;
+	//basic roll of a single die
+	this.rollDie = function(d) {
+		return Math.floor(Math.random()*d)+1;
+	}
+
+	//roll of a number of dice in the format 3d6+2, e.g.
+	this.complexRoll = function(numDice,d,mod) {
+		var total = 0;
+		var numDice = numDice;
+		for (var i = numDice; i > 0; i--) {
+			var roll = this.rollDie(d);
+			if (roll === d) {
+				i++;
+			}
+			total += roll;
 		}
-		total += roll;
+		return total+mod;
 	}
-	return total+mod;
+
+	//figure out an average roll given x rolls
+	this.avgRoll = function(rolls,numDice,d,mod) {
+		var total = 0;
+		for (var i = rolls; i > 0; i--) {
+			var thisRoll = this.complexRoll(numDice,d,mod);
+			if (thisRoll > this.highRoll) {
+				this.highRoll = thisRoll;
+			}
+			total += thisRoll;
+		}
+		return total / rolls;
+	}
 }
 
-//figure out an average roll given x rolls
-var avgRoll = function(rolls,numDice,d,mod) {
-	var total = 0;
-	for (var i = rolls; i > 0; i--) {
-		total += complexRoll(numDice,d,mod);
-	}
-	return total / rolls;
-}
+var roller = new MyRoll();
 
-alert(avgRoll(100000,3,6,0));
+alert(roller.avgRoll(100,1,6,0));
